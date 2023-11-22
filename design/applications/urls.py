f229001b36176c81ca2578e57506034864c3aa37
alPumpur <aleftinapumpur@gmail.com>
2023-11-22 12:43:03 +0700

    возможность зарегистрироваться и войти

--- design/applications/urls.py
+++ design/applications/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('', include('django.contrib.auth.urls')),
-    # path('register/', views.RegisterView.as_view(), name='register')
+    path('register/', views.RegisterView.as_view(), name='register')
 ]
